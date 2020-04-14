6669bf89077a7157707aa417279fc6b821ad000b
aseleidy <ash.etemadi@gmail.com>
2020-04-14 11:54:58 -0400

    run.rb and user class edits up until programming_language

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,20 @@
 require_relative '../config/environment'
 
+puts "Welcome to the Dev Jobs Portal!" "\n"
+puts "Please enter your fabulous name."
+name = gets.chomp "\n"
 
+puts "Please enter your email address."
+email = gets.chomp "\n"
+puts ""
+User.has_account?(name, email) "\n"
 
-puts "HELLO WORLD"
+puts "Which programming language would you like to get a job in?"
+programming_language = gets.chomp
+
+puts "Which city would you like to work in?"
+city = gets.chomp
+# self.city = city 
+
+# User.find_by(programming_language: programming_language, location: city)
+
