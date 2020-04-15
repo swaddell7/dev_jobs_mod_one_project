186de5639188840f1531272a44b6ccee217273ea
swaddell7 <stefani.waddell@hotmail.com>
2020-04-15 04:51:16 -0500

    created view all applications for user method

--- lib/application.rb
+++ lib/application.rb
@@ -2,6 +2,19 @@ class Application < ActiveRecord::Base
   belongs_to :user
   belongs_to :job
 
-  
+
+
+  #still trying to figure out why this method returns back an array of just the application instances..??
+  def self.view_all_for_user(name, email)
+    user_applications = self.all.where(user_id: User.find_by(name: name, email_address: email).id)
+    user_applications.each do |application|
+      puts "Title: #{application.job["title"]}"
+      puts "Company: #{application.job["company"]}"
+      if application.job["company_url"]
+          puts "Company URL: #{application.job["company_url"]}"
+      end
+      puts "Description: #{application.job["description"]}"
+    end
+  end
   
 end
